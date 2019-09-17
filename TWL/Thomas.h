b8973428f3190351ac8608c82a7b04cbb3421727
Letícia Pasdiora <leticiapasdiora@gmail.com>
2019-09-16 23:52:40 -0400

    First commit

---
+++ TWL/Thomas.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "PlayableCharacter.h"
+
+class Thomas : public PlayableCharacter
+{
+public:
+	// A constructor specific to Thomas
+	Thomas();
+
+	// The overriden input handler for Thomas
+	bool virtual handleInput();
+};
